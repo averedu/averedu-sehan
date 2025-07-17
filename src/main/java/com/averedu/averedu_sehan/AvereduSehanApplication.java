873b24b486fce058fff37b91ce30460d42c6bb84
lyj150515 <lyj150515@gmail.com>
2025-07-17 11:04:03 +0900

    war 추출을 위한 수정

--- src/main/java/com/averedu/averedu_sehan/AvereduSehanApplication.java
+++ src/main/java/com/averedu/averedu_sehan/AvereduSehanApplication.java
@@ -2,12 +2,19 @@ package com.averedu.averedu_sehan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AvereduSehanApplication {
+public class AvereduSehanApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AvereduSehanApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AvereduSehanApplication.class);
 	}
 
 }
