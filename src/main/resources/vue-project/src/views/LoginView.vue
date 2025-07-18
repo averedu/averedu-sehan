c7e12ec8807e4c3323a59a5fa73072d08be99cbc
lyj150515 <lyj150515@gmail.com>
2025-07-18 11:15:47 +0900

    로그인 수정

--- src/main/resources/vue-project/src/views/LoginView.vue
+++ src/main/resources/vue-project/src/views/LoginView.vue
@@ -1,21 +1,47 @@
 <template>
-<section class="bg-gray-50 dark:bg-gray-900"> 
-  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  
-              </h1>
-              <form class="space-y-4 md:space-y-6" action="#">
-                  <div>
-                      <label for="id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">아이디</label>
-                      <input v-model="login.loginId" type="id" name="id" id="id"  value="" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="아이디를 입력해주세요." required="">
-                  </div>
-                  <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">패스워드</label>
-                      <input v-model="login.pwd" type="password" name="password" id="password" value=""  placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                  </div>
-                  <!--
+  <section class="bg-gray-50 dark:bg-gray-900">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div
+        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1
+            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+          ></h1>
+          <form class="space-y-4 md:space-y-6" action="#">
+            <div>
+              <label for="id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >아이디</label
+              >
+              <input
+                v-model="login.loginId"
+                type="id"
+                name="id"
+                id="id"
+                value=""
+                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="아이디를 입력해주세요."
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                for="password"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >패스워드</label
+              >
+              <input
+                v-model="login.pwd"
+                type="password"
+                name="password"
+                id="password"
+                value=""
+                placeholder="••••••••"
+                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            <!--
                   <div class="flex items-center justify-between">
                       <div class="flex items-start">
                           <div class="flex items-center h-5">
@@ -28,72 +54,70 @@
                       <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">패스워드를 잊으셨나요?</a>
                   </div>
                   -->
-                  <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                  @click="loginFun">로그인</button>
-                  <!--
+            <button
+              type="button"
+              class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              @click="loginFun"
+            >
+              로그인
+            </button>
+            <!--
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                       <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">회원가입</a>
                   </p>-->
-              </form>
-          </div>
+          </form>
+        </div>
       </div>
-  </div>
-</section>
-
+    </div>
+  </section>
 </template>
 
 <script setup>
-import { useRouter } from "vue-router";
-import { ref } from 'vue';
-import axios from 'axios';
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import axios from 'axios'
 
-const router = useRouter();
+const router = useRouter()
 
 let login = ref({
-    loginId : '',
-    pwd : ''
+  loginId: '',
+  pwd: '',
 })
 
-
-
 const loginFun = () => {
-   
-    if(login.value.loginId === ''){
-        alert('아이디를 입력 해주세요.');
-        return false;
-    }
+  if (login.value.loginId === '') {
+    alert('아이디를 입력 해주세요.')
+    return false
+  }
 
-    if(login.value.pwd === ''){
-        alert('패스워드를 입력 해주세요.');
-        return false;
-    }
+  if (login.value.pwd === '') {
+    alert('패스워드를 입력 해주세요.')
+    return false
+  }
 
-    const obj = Object.assign({}, login.value);
+  const obj = Object.assign({}, login.value)
 
-     axios
-    .post("/restApi/login", obj) 
-    .then(restApi => {
-      console.log('restApi : ', restApi);
+  axios
+    .post('/restApi/login', obj)
+    .then((restApi) => {
+      console.log('restApi : ', restApi)
       if (restApi.data.statusCd === 50) {
-          alert(restApi.data.statusMsg);
-          return false;
+        alert(restApi.data.statusMsg)
+        return false
       }
-      if (restApi.data.statusCd === 500){
-          alert(restApi.data.statusMsg);
-          return false;
+      if (restApi.data.statusCd === 500) {
+        alert(restApi.data.statusMsg)
+        return false
       }
-      if (restApi.data.statusCd === 200){
-          alert(restApi.data.statusMsg);
-          router.push('/main');
+      if (restApi.data.statusCd === 200) {
+        // alert(restApi.data.statusMsg)
+        router.push('/main')
       }
     })
-    .catch(error => {
-      console.log('login error', error);
+    .catch((error) => {
+      console.log('login error', error)
     })
-
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
