d817af89369e27d053f692eecc261c9b036c08d4
lyj150515 <lyj150515@gmail.com>
2025-07-18 11:12:26 +0900

    헤더 상단 로그아웃

--- src/main/java/com/averedu/averedu_sehan/controller/HomeController.java
+++ src/main/java/com/averedu/averedu_sehan/controller/HomeController.java
@@ -3,6 +3,8 @@ package com.averedu.averedu_sehan.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class HomeController {
 	
@@ -10,5 +12,11 @@ public class HomeController {
 	public String index() {
 		 // return "redirect:/index.html";
 		return "index.html";
+	}
+	
+	@GetMapping(value = "/logout")
+	public String logout(HttpServletRequest request) {	
+		request.getSession().invalidate();
+		return "redirect:/";
 	}
 }
--- src/main/resources/vue-project/src/components/Header.vue
+++ src/main/resources/vue-project/src/components/Header.vue
@@ -1,26 +1,17 @@
 <script setup>
 import { ref } from 'vue'
 import { useSidebar } from '../composables/useSidebar'
+import { useRouter } from 'vue-router'
 
 const dropdownOpen = ref(false)
 const { isOpen } = useSidebar()
 </script>
 
 <template>
-  <header
-    class="flex items-center justify-between px-6 py-4 bg-white border-b-4 border-indigo-600"
-  >
+  <header class="flex items-center justify-between px-6 py-4 bg-white border-b-4 border-indigo-600">
     <div class="flex items-center">
-      <button
-        class="text-gray-500 focus:outline-none lg:hidden"
-        @click="isOpen = true"
-      >
-        <svg
-          class="w-6 h-6"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+      <button class="text-gray-500 focus:outline-none lg:hidden" @click="isOpen = true">
+        <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M4 6H20M4 12H20M4 18H11"
             stroke="currentColor"
@@ -31,7 +22,9 @@ const { isOpen } = useSidebar()
         </svg>
       </button>
 
-      <h3 class="text-3xl font-medium text-gray-700">{{$route.name}}</h3>
+      <h3 class="text-3xl font-medium text-gray-700">{{ $route.name }}</h3>
     </div>
+    <!-- <p class="text-right cursor-pointer">로그아웃</p> -->
+    <a href="/logout">로그아웃</a>
   </header>
 </template>
