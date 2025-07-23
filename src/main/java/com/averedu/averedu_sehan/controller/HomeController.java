7a8ff7c7905707cf9c77ab6041acf56cf391e7d0
lyj150515 <lyj150515@gmail.com>
2025-07-23 09:15:45 +0900

    로그아웃 수정

--- src/main/java/com/averedu/averedu_sehan/controller/HomeController.java
+++ src/main/java/com/averedu/averedu_sehan/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -15,8 +16,12 @@ public class HomeController {
 	}
 	
 	@GetMapping(value = "/logout")
-	public String logout(HttpServletRequest request) {	
-		request.getSession().invalidate();
+	public String logout(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		
+		if(session != null) {
+			session.invalidate();
+		}
 		return "redirect:/";
 	}
 }
